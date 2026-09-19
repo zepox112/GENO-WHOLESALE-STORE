88b170e9cbef8ecea6654a6beb99c1baaaa1870c
Zepox OG <zepoxkenny4@gmail.com>
2026-09-19 16:37:17 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'app-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png'
+];
+
+// Kupakua na kuhifadhi faili wakati wa kusanikisha (Install)
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Kuhudumia maombi ya mtandao au kutumia cache wakati hakuna intaneti
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
