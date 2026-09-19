3c694c76c331992904ebcb1b0d58786c065f3341
Zepox OG <zepoxkenny4@gmail.com>
2026-09-19 17:00:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = 'app-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
